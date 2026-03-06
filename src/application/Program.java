f23b574799c340e3cd4d0ccc894bd1679e9ccecd
Marcelo Parabocz <macerno@gmail.com>
2026-03-06 16:23:05 -0300

    feat: piece movement implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,15 +1,29 @@
 package application;
 
+import java.util.Scanner;
+
 import chess.ChessMatch;
+import chess.ChessPiece;
+import chess.ChessPosition;
 
 public class Program {
 
 	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
 		System.out.println("This is almost working already /s\n");
 
 		ChessMatch match = new ChessMatch();
-		
-		UI.printBoard(match.getPieces());
+
+		while (true) {
+			UI.printBoard(match.getPieces());
+			System.out.println("\nSource: ");
+			ChessPosition source = UI.readChessPosition(sc);
+
+			System.out.println("\nTarget: ");
+			ChessPosition target = UI.readChessPosition(sc);
+
+			ChessPiece capturedPiece = match.performChessMove(source, target);
+		}
 
 	}
 
