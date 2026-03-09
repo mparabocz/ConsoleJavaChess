1eed4a5317e2cfadef764a80d3c8d2e97b0a65ee
Marcelo Parabocz <macerno@gmail.com>
2026-03-09 10:40:06 -0300

    feat: initial implementation of possible moves for pieces

--- src/application/UI.java
+++ src/application/UI.java
@@ -28,6 +28,7 @@ public class UI {
 	public static final String ANSI_CYAN_BACKGROUND = "\u001B[46m";
 	public static final String ANSI_WHITE_BACKGROUND = "\u001B[47m";
 
+	//Does not work on the IDE console.
 	public static void clearScreen() {
 		System.out.print("\033[H\033[2J");
 		System.out.flush();
--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -38,6 +38,9 @@ public class ChessMatch {
 		if (!board.thereIsAPiece(position)) {
 			throw new ChessException("There is no piece on the source position.");
 		}
+		if (!board.piece(position).isThereAnyPossibleMove()) {
+			throw new ChessException("There are no possible moves for the chosen piece");
+		}
 	}
 
 	private Piece makeMove(Position source, Position target) {
